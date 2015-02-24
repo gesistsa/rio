ff9a186544533e31a72d77c70746dafdb06c4679
leeper <thosjleeper@gmail.com>
2015-02-24 21:37:44 +0100

    migrate some methods to private funcs (closes #6)

--- R/export.R
+++ R/export.R
@@ -1,3 +1,17 @@
+export.txt <- function(x, file, sep = "\t", row.names = FALSE, header = TRUE, ...) {
+    write.table(x, file = file, sep = sep, row.names = row.names, col.names = header, ...)
+}
+
+export.clipboard <- function(x, ...) {
+    if(Sys.info()["sysname"] == "Darwin") {
+            clip <- pipe("pbcopy", "w")                                             
+            write.table(x, file = clip, sep="\t", row.names=row.names, col.names=header, ...)
+            close(clip)
+    } else if(Sys.info()["sysname"] == "Windows") {
+            write.table(x, file="clipboard", sep="\t", row.names=row.names, col.names=header, ...)
+    }
+}
+
 export <- function(x, file="", format=NULL, row.names=FALSE, header=TRUE, ... ) {
     if (!is.data.frame(x) & !is.matrix(x)) {
         stop("x is not a data frame or matrix.")
@@ -8,26 +22,18 @@ export <- function(x, file="", format=NULL, row.names=FALSE, header=TRUE, ... ) 
     if(is.null(format))
         format <- .guess(file)
     switch(format,
-         txt = write.table(x, file=file, sep="\t", row.names=row.names, col.names=header, ...),
-         tsv = write.table(x, file=file, sep="\t", row.names=row.names, col.names=header, ...),
-         clipboard = {
-                if(Sys.info()["sysname"] == "Darwin") {
-                        clip <- pipe("pbcopy", "w")                                             
-                        write.table(x, file = clip, sep="\t", row.names=row.names, col.names=header, ...)
-                        close(clip)
-                } else if(Sys.info()["sysname"] == "Windows") {
-                        write.table(x, file="clipboard", sep="\t", row.names=row.names, col.names=header, ...)
-                }
-         },
+         txt = export.txt(x, file=file, sep="\t", row.names=row.names, col.names=header, ...),
+         tsv = export.txt(x, file=file, sep="\t", row.names=row.names, col.names=header, ...),
+         clipboard = export.clipboard(x, ...),
          rds = saveRDS(x, file=file, ...),
          csv = write.csv(x, file=file, row.names=row.names, ...), 
          rdata = save(x, file = file, ...),
-         sav = haven::write_sav(data = x, path = file),
-         dta = haven::write_dta(data = x, path = file),
-         dbf = foreign::write.dbf(dataframe = x, file = file, ...),
-         json = cat(jsonlite::toJSON(x, ...), file = file),
-         arff = foreign::write.arff(x = x, file = file, ...),
-         xlsx = openxlsx::write.xlsx(x = x, file = file, ...),
+         sav = write_sav(data = x, path = file),
+         dta = write_dta(data = x, path = file),
+         dbf = write.dbf(dataframe = x, file = file, ...),
+         json = cat(toJSON(x, ...), file = file),
+         arff = write.arff(x = x, file = file, ...),
+         xlsx = write.xlsx(x = x, file = file, ...),
          stop("Unknown file format")
          )
     invisible(file)
