ac016c0304152206943996113f7e08746d85f8a2
leeper <thosjleeper@gmail.com>
2015-12-23 20:11:34 +0000

    add convert test suite (#51)

---
+++ tests/testthat/convert.R
@@ -0,0 +1,18 @@
+context("Convert")
+
+test_that("Basic file conversion", {
+    export(iris, "iris.dta")
+    convert("iris.dta", "iris.csv")
+    convert("iris.csv", "iris.dta")
+    x <- import("iris.dta")
+    expect_true(identical(names(iris), names(x)))
+    expect_true(identical(dim(iris), dim(x)))
+})
+
+test_that("File conversion with arguments", {
+    export(iris, "iris.csv", format = "tsv")
+    convert("iris.csv", "iris.csv", in_opts = list(format = "tsv"))
+    convert("iris.csv", "iris.tsv",
+            in_opts = list(format = "tsv"), out_opts = list(format = "csv"))
+})
+
