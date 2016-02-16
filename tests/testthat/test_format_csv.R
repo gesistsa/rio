e9d35de204f276e40a61730e9e34e39c4831dd54
Thomas J. Leeper <thosjleeper@gmail.com>
2016-02-16 16:59:53 +0000

    add csv2 tests (#51)

--- tests/testthat/test_format_csv.R
+++ tests/testthat/test_format_csv.R
@@ -11,4 +11,15 @@ test_that("Import from CSV", {
     expect_that(colnames(noheadercsv)[1], equals("V1"), label = "Header is correctly specified")
 })
 
+context("CSV (.csv2) imports/exports")
+
+test_that("Export to CSV", {
+    expect_true(export(iris, "iris.csv", format = "csv2") %in% dir())
+})
+
+test_that("Import from CSV", {
+    expect_error(is.data.frame(import("iris.csv")))
+    expect_true(is.data.frame(import("iris.csv", format = "csv2")))
+})
+
 unlink("iris.csv")
