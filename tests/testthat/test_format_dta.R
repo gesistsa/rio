8c4a39e35728acb795174930b598cd88f5c7f023
Thomas J. Leeper <thosjleeper@gmail.com>
2016-03-10 10:14:47 +0000

    make sav and dta tests pkg-specific

--- tests/testthat/test_format_dta.R
+++ tests/testthat/test_format_dta.R
@@ -5,8 +5,12 @@ test_that("Export to Stata", {
     expect_true(export(iris, "iris.dta") %in% dir())
 })
 
-test_that("Import from Stata", {
-    expect_true(is.data.frame(import("iris.dta")))
+test_that("Import from Stata (read_dta)", {
+    expect_true(is.data.frame(import("iris.dta", haven = TRUE)))
+})
+
+test_that("Import from Stata (read.dta)", {
+    expect_true(is.data.frame(import("iris.dta", haven = FALSE)))
 })
 
 unlink("iris.dta")
--- tests/testthat/test_format_sav.R
+++ tests/testthat/test_format_sav.R
@@ -5,8 +5,12 @@ test_that("Export to SPSS (.sav)", {
     expect_true(export(iris, "iris.sav") %in% dir())
 })
 
-test_that("Import from SPSS (.sav)", {
-    expect_true(is.data.frame(import("iris.sav")))
+#test_that("Import from SPSS (.sav; read.spss)", {
+#    expect_true(is.data.frame(import("iris.sav", haven = FALSE)))
+#})
+
+test_that("Import from SPSS (.sav; read_sav)", {
+    expect_true(is.data.frame(import("iris.sav", haven = TRUE)))
 })
 
 unlink("iris.sav")
