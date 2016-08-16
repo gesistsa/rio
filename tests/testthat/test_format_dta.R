3e61b2984bb7b4aa3163a25cedc5963dea437ddd
Thomas J. Leeper <thosjleeper@gmail.com>
2016-08-16 10:11:08 +0100

    fix failing Stata export test (closes #113)

--- tests/testthat/test_format_dta.R
+++ tests/testthat/test_format_dta.R
@@ -2,17 +2,19 @@ context("Stata (.dta) imports/exports")
 require("datasets")
 
 test_that("Export to Stata", {
-    expect_true(export(iris, "iris.dta") %in% dir())
+    expect_true(export(mtcars, "mtcars.dta") %in% dir())
+    expect_error(export(iris, "iris.dta"), label = "Export fails on invalid Stata names")
 })
 
 test_that("Import from Stata (read_dta)", {
-    expect_true(is.data.frame(import("iris.dta", haven = TRUE)))
+    expect_true(is.data.frame(import("mtcars.dta", haven = TRUE)))
     # arguments ignored
-    expect_warning(is.data.frame(import("iris.dta", haven = TRUE, extraneous.argument = TRUE)))
+    expect_warning(is.data.frame(import("mtcars.dta", haven = TRUE, extraneous.argument = TRUE)))
 })
 
 test_that("Import from Stata (read.dta)", {
-    expect_true(is.data.frame(import("iris.dta", haven = FALSE)))
+    expect_true(is.data.frame(import("mtcars.dta", haven = FALSE)))
 })
 
+unlink("mtcars.dta")
 unlink("iris.dta")
