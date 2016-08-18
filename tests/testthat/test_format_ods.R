eb7f23b32cbd17337aa877a998645385b364952e
Thomas J. Leeper <thosjleeper@gmail.com>
2016-08-18 14:48:30 +0100

    add ODS import tests and ex file

--- tests/testthat/test_format_ods.R
+++ tests/testthat/test_format_ods.R
@@ -1,4 +1,9 @@
 context("ODS imports/exports")
 require("datasets")
 
-#test_that("Import from ODS", {})
+test_that("Import from ODS", {
+    ods <- import(system.file("examples", "mtcars.ods", package = "rio"))
+    expect_true(is.data.frame(ods), label = "ODS import returns data.frame")
+    expect_true(identical(names(mtcars), names(ods)), label = "ODS import returns correct names")
+    expect_true(identical(dim(mtcars), dim(ods)), label = "ODS import returns correct dimensions")
+})
