7905354e2a316b4c187166a6c1cff5d45b82d3c9
Thomas J. Leeper <thosjleeper@gmail.com>
2017-02-14 09:14:17 +0000

    add examples to main ? rio page

--- R/rio.R
+++ R/rio.R
@@ -1,7 +1,26 @@
 #' @docType package
 #' @name rio
 #' @title A Swiss-Army Knife for Data I/O
-#' @description The aim of rio is to make data file I/O in R as easy as possible. \code{\link{export}} and \code{\link{import}} serve as a Swiss-army knife for painless data I/O for data from almost any file format by inferring the data structure from the file extension, natively reading web-based data sources, setting reasonable defaults for import and export, and relying on efficient data import and export packages. An additional convenience function, \code{\link{convert}}, provides a simple method for converting between file types.
+#' @description The aim of rio is to make data file input and output as easy as possible. \code{\link{export}} and \code{\link{import}} serve as a Swiss-army knife for painless data I/O for data from almost any file format by inferring the data structure from the file extension, natively reading web-based data sources, setting reasonable defaults for import and export, and relying on efficient data import and export packages. An additional convenience function, \code{\link{convert}}, provides a simple method for converting between file types.
+#' 
+#' @examples
+#' # export
+#' library("datasets")
+#' export(mtcars, "mtcars.csv") # comma-separated values
+#' export(mtcars, "mtcars.rds") # R serialized
+#' export(mtcars, "mtcars.sav") # SPSS
+#' 
+#' # import
+#' x <- import("mtcars.csv")
+#' y <- import("mtcars.rds")
+#' z <- import("mtcars.sav")
+#' 
+#' # convert
+#' convert("mtcars.sav", "mtcars.dta")
+#' 
+#' # cleanup
+#' unlink(c("mtcars.csv", "mtcars.rds", "mtcars.sav", "mtcars.dta"))
+#' 
 #' \href{https://github.com/Stan125/GREA}{GREA} provides an RStudio add-in to import data using rio.
 #' @seealso \code{\link{import}}, \code{\link{export}}, \code{\link{convert}}
 NULL 
