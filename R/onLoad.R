093a9cb7e0c807d86093db0c6ad2077416af3e77
Thomas J. Leeper <thosjleeper@gmail.com>
2017-03-02 17:48:48 +0000

    !

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,7 +5,7 @@
 .onAttach <- function(libname, pkgname) {
     if (interactive()) {
         w <- suggestions %in% installed.packages()[ , 1, drop = TRUE]
-        if (any(w)) {
+        if (any(!w)) {
             msg <- "The following rio suggested packages are not installed: %s\nUse 'install_formats()' to install them"
             packageStartupMessage(sprintf(msg, paste0(sQuote(suggestions[!w]), collapse = ", ")))
         }
