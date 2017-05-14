a7c601710b7d96e6001df46184f2c9c868916c2f
Thomas J. Leeper <thosjleeper@gmail.com>
2017-05-14 15:18:08 +0100

    add spread_attrs() function

--- R/gather_attrs.R
+++ R/gather_attrs.R
@@ -1,8 +1,9 @@
-#' @title Gather attributes from data.frame variables
-#' @description This function moves variable-level attributes to the data.frame level
-#' @details \code{\link{import}} attempts to standardize the return value from the various import functions to the extent possible, thus providing a uniform data structure regardless of what import package or function is used. It achieves this by storing any optional variable-related attributes at the variable level (i.e., an attribute for \code{mtcars$mpg} is stored in \code{attributes(mtcars$mpg)} rather than \code{attributes(mtcars)}). \code{gather_attrs} moves these to the data frame level (i.e., in \code{attributes(mtcars)}).
-#' @param x A data.frame.
-#' @return \code{x}, with variable-level attributes stored at the data.frame level.
+#' @rdname gather_attrs
+#' @title Gather attributes from data frame variables
+#' @description \code{gather_attrs} moves variable-level attributes to the data frame level and \code{spread_attrs} reverses that operation.
+#' @details \code{\link{import}} attempts to standardize the return value from the various import functions to the extent possible, thus providing a uniform data structure regardless of what import package or function is used. It achieves this by storing any optional variable-related attributes at the variable level (i.e., an attribute for \code{mtcars$mpg} is stored in \code{attributes(mtcars$mpg)} rather than \code{attributes(mtcars)}). \code{gather_attrs} moves these to the data frame level (i.e., in \code{attributes(mtcars)}). \code{spread_attrs} moves attributes back to the variable level.
+#' @param x A data frame.
+#' @return \code{x}, with variable-level attributes stored at the data frame level.
 #' @examples
 #' e <- import("http://www.stata-press.com/data/r13/auto.dta")
 #' str(e)
@@ -16,6 +17,9 @@ gather_attrs <- function(x) {
         stop("'x' is not a data.frame")
     }
     dfattrs <- attributes(x)
+    if ("label" %in% names(dfattrs)) {
+        names(dfattrs)[names(dfattrs) == "label"] <- "title"
+    }
     varattrs <- rep(list(list()), length(x))
     for (i in seq_along(x)) {
         a <- attributes(x[[i]])
@@ -29,5 +33,25 @@ gather_attrs <- function(x) {
         }), attrnames)
         attributes(x) <- c(dfattrs, outattrs)
     }
+    x
+}
+
+#' @rdname gather_attrs
+#' @export
+spread_attrs <- function(x) {
+    if (!inherits(x, "data.frame")) {
+        stop("'x' is not a data.frame")
+    }
+    dfattrs <- attributes(x)
+    d_level_attrs <- names(dfattrs) %in% c("row.names", "class", "names", "notes", "title")
+    varattrs <- dfattrs[!d_level_attrs]
+    for (i in seq_along(x)) {
+        a <- attributes(x[[i]])
+        attributes(x[[i]]) <- c(a, lapply(varattrs, `[[`, i))
+    }
+    if ("title" %in% names(dfattrs)) {
+        names(dfattrs)[names(dfattrs) == "title"] <- "label"
+    }
+    attributes(x) <- dfattrs[d_level_attrs]
     x
 }
--- tests/testthat/test_gather_attrs.R
+++ tests/testthat/test_gather_attrs.R
@@ -1,16 +1,22 @@
 context("Gather attrs")
 
+e <- import("http://www.stata-press.com/data/r13/auto.dta")
+
 test_that("Gather attrs from Stata", {
-    e <- import("http://www.stata-press.com/data/r13/auto.dta")
     g <- gather_attrs(e)
     expect_true(length(attributes(e[[1]])) >= 1)
     expect_true(length(attributes(g[[1]])) == 0)
     expect_true(length(attributes(e)) == 5)
-    expect_true(length(attributes(g)) == 7)
+    expect_true(length(attributes(g)) == 8)
     expect_true("label" %in% names(attributes(e[[1]])))
     expect_true(!"label" %in% names(attributes(g[[1]])))
     expect_true("label" %in% names(attributes(g)))
     expect_true("labels" %in% names(attributes(g)))
+})
+
+test_that("Spread gathered attributes", {
+    g <- gather_attrs(e)
+    expect_true(all.equal(spread_attrs(g), e, check.attributes = TRUE))
 })
 
 test_that("Gather empty attributes", {
@@ -21,3 +27,9 @@ test_that("Gather empty attributes", {
     expect_true(length(attributes(iris)) == 3)
     expect_true(length(attributes(g)) == 3)
 })
+
+test_that("gather_attrs() fails on non-data frame", {
+    expect_error(gather_attrs(letters))
+})
+
+rm(e)
