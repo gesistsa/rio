a2cb709fcd8617ece05bf43ede1c4b3fd6a08e30
Ryan Price <ryapric@gmail.com>
2018-07-06 16:34:16 -0500

    Add more robust internals & tests to `install_formats()`. Other small changes noted in `NEWS.md`.

--- tests/testthat/test_install_formats.R
+++ tests/testthat/test_install_formats.R
@@ -1,6 +1,21 @@
 context("Install uninstalled formats")
 
-test_that("install_formats()", {
+test_that("uninstalled_formats()", {
     skip_on_cran()
-    expect_true(install_formats())
+    formats <- uninstalled_formats()
+    if (is.null(formats)) {
+      expect_true(install_formats())
+    } else {
+      expect_type(formats, "character")
+    }
+})
+
+test_that("install_formats()", {
+  suggestions <- read.dcf(system.file("examples/example-DESCRIPTION",
+                                      package = "rio", mustWork = TRUE),
+                          fields = "Suggests")
+  suggestions <- parse_suggestions(suggestions)
+  expect_true("NANTUCKET" %in% suggestions)
+  expect_true("readODS" %in% suggestions)
+  expect_false("devtools" %in% suggestions)
 })
