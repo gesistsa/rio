714a77feca8cb54300c959e54615f7212f0a815e
Ryan Price <ryan.price@slalom.com>
2018-07-13 14:29:54 +0000

    Indent spacing adjustment.

--- tests/testthat/test_install_formats.R
+++ tests/testthat/test_install_formats.R
@@ -4,18 +4,18 @@ test_that("uninstalled_formats()", {
     skip_on_cran()
     formats <- uninstalled_formats()
     if (is.null(formats)) {
-      expect_true(install_formats())
+        expect_true(install_formats())
     } else {
-      expect_type(formats, "character")
+        expect_type(formats, "character")
     }
 })
 
 test_that("install_formats()", {
-  suggestions <- read.dcf(system.file("examples/example-DESCRIPTION",
-                                      package = "rio", mustWork = TRUE),
-                          fields = "Suggests")
-  suggestions <- parse_suggestions(suggestions)
-  expect_true("NANTUCKET" %in% suggestions)
-  expect_true("readODS" %in% suggestions)
-  expect_false("devtools" %in% suggestions)
+    suggestions <- read.dcf(system.file("examples/example-DESCRIPTION",
+                                        package = "rio", mustWork = TRUE),
+                            fields = "Suggests")
+    suggestions <- parse_suggestions(suggestions)
+    expect_true("NANTUCKET" %in% suggestions)
+    expect_true("readODS" %in% suggestions)
+    expect_false("devtools" %in% suggestions)
 })
