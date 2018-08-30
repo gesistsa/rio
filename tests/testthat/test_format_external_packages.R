15263ab63f61442bdd4dd816a1f40ee37b7b2783
Trevor Davis <trevor.l.davis@gmail.com>
2018-08-30 12:36:00 -0700

    Just a simple expect_error check

--- tests/testthat/test_format_external_packages.R
+++ tests/testthat/test_format_external_packages.R
@@ -1,51 +1,23 @@
-context("External package support Import/Export")
+context("External package Import/Export support")
 require("datasets")
 
-
-extensions <- c("bib", "bmp", "gexf", "gnumeric", "jpeg", "jpg", "npy", "png", "sss", "sdmx", "tiff")
-read_fn <- function(ext) {
-    switch(ext,
-          bib = "bib2df::bib2df",
-          bmp = "bmp::read.bmp",
-          gexf = "rgexf::read.gexf",
-          gnumeric = "gnumeric::read.gnumeric.sheet",
-          jpeg = "jpeg::readJPEG",
-          jpg = "jpeg::readJPEG",
-          npy = "RcppCNPy::npyLoad",
-          png = "png::readPNG",
-          sdmx = "sdmx::readSDMX",
-          sss = "sss::read.sss",
-          tiff = "tiff::readTIFF")
-}
-
 test_that("External read functions without an import method", {
-    message <- "%s format not supported. Consider using the '%s()' function"
+    extensions <- c("bib", "bmp", "gexf", "gnumeric", "jpeg", "jpg", "npy", "png", "sss", "sdmx", "tiff")
     for (ext in extensions) {
         file <- file.path(tempdir(), paste0("test.", ext))
         file.create(file)
         on.exit(unlink(file))
-        expect_error(import(file), sprintf(message, ext, read_fn(ext)), fixed=TRUE)
+        expect_error(import(file))
     }
 })
 
-i_extensions <- c("bean", "beancount", "ledger", "hledger")
-i_package_fn <- function(ext) {
-    switch(ext,
-        bean = "ledger",
-        beancount = "ledger",
-        ledger = "ledger",
-        hledger = "ledger")
-}
-
-test_that("External import functions", {
-    message <- paste("Import support for the %s format is exported by the %s package.",
-                           "Run 'library(%s)' then try again.")
-    for (ext in i_extensions) {
+test_that("import method exported by an external package", {
+    extensions <- c("bean", "beancount", "ledger", "hledger")
+    for (ext in extensions) {
         file <- file.path(tempdir(), paste0("test.", ext))
         file.create(file)
         on.exit(unlink(file))
-        package <- i_package_fn(ext)
-        expect_error(import(file), sprintf(message, ext, package, package), fixed=TRUE)
+        expect_error(import(file))
     }
 })
 
