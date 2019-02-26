75ab0f4913d03784505ca123fe92edbd4ade0a86
Thomas J. Leeper <thosjleeper@gmail.com>
2019-02-26 14:40:00 +0000

    suppress warnings on eviews test (closes #189)

--- tests/testthat/test_format_eviews.R
+++ tests/testthat/test_format_eviews.R
@@ -2,6 +2,6 @@ context("EViews import")
 
 if (requireNamespace("hexView")) {
     test_that("Import from EViews", {
-        expect_true(is.data.frame(import(hexView::hexViewFile("data4-1.wf1"))))
+        expect_true(is.data.frame(suppressWarnings(import(hexView::hexViewFile("data4-1.wf1")))))
     })
 }
