549b0d440b54eba11190a1c17df3e7f7ca4adc35
Bill Denney <wdenney@humanpredictions.com>
2019-06-27 12:53:39 -0400

    Give message if pzfx test is skipped due to missing library

--- tests/testthat/test_format_pzfx.R
+++ tests/testthat/test_format_pzfx.R
@@ -1,20 +1,20 @@
 context("pzfx imports/exports")
 require("datasets")
 
-if (requireNamespace("pzfx")) {
-  iris_numeric <- iris
-  iris_numeric$Species <- as.numeric(iris_numeric$Species)
-  test_that("Export to pzfx", {
-    expect_true(export(iris_numeric, "iris.pzfx") %in% dir())
-  })
+iris_numeric <- iris
+iris_numeric$Species <- as.numeric(iris_numeric$Species)
+test_that("Export to pzfx", {
+  skip_if_not_installed(pkg="pzfx")
+  expect_true(export(iris_numeric, "iris.pzfx") %in% dir())
+})
 
-  test_that("Import from pzfx", {
-    expect_true(is.data.frame(import("iris.pzfx")))
-    # Note that the dim test is only true as long as the data are exported with
-    # write_pzfx(..., row_names=FALSE) which is the default in the export
-    # method, but it is not default in pzfx::write_pzfx()
-    expect_true(identical(dim(import("iris.pzfx")), dim(iris_numeric)))
-  })
+test_that("Import from pzfx", {
+  skip_if_not_installed(pkg="pzfx")
+  expect_true(is.data.frame(import("iris.pzfx")))
+  # Note that the dim test is only true as long as the data are exported with
+  # write_pzfx(..., row_names=FALSE) which is the default in the export
+  # method, but it is not default in pzfx::write_pzfx()
+  expect_true(identical(dim(import("iris.pzfx")), dim(iris_numeric)))
+})
 
-  unlink("iris.pzfx")
-}
+unlink("iris.pzfx")
