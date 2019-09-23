3e186f4f31b42925193281972dab959b65316064
Alex Bokov <bokov@uthscsa.edu>
2019-09-23 09:05:40 -0500

    Added dynamic argument checking for 'readODS:read_ods()' as proposed in #223. Also more detailed tests.

--- tests/testthat/test_format_ods.R
+++ tests/testthat/test_format_ods.R
@@ -3,10 +3,22 @@ require("datasets")
 
 test_that("Import from ODS", {
     skip_if_not_installed(pkg="readODS")
-    ods <- import(system.file("examples", "mtcars.ods", package = "rio"))
+    expect_message(ods0 <- import(system.file("examples", "mtcars.ods",
+                                              package = "rio")),
+                   'Parsed with column specification:',
+                   label = "ODS import with default arguments works")
+    expect_warning(ods <- import(system.file("examples", "mtcars.ods"
+                                             , package = "rio"),
+                                 sheet = 1, col_names = TRUE,
+                                 path = 'ignored value',
+                                 invalid_argument = 42),
+                   "The following arguments were ignored for read_ods:\ninvalid_argument, path",
+                   label = "ODS import ignores redundant and unknown arguments with a warning")
+    expect_identical(ods0,ods, label = "ODS import ignored arguments don't affect output")
     expect_true(is.data.frame(ods), label = "ODS import returns data.frame")
     expect_true(identical(names(mtcars), names(ods)), label = "ODS import returns correct names")
     expect_true(identical(dim(mtcars), dim(ods)), label = "ODS import returns correct dimensions")
+    expect_equivalent(ods,mtcars,label = "ODS import returns correct values")
 })
 
 test_that("Export to ODS", {
