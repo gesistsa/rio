259f093b97f8dcfc6d0fd13aed08168f1d19eb88
Alex Bokov <bokov@uthscsa.edu>
2019-09-26 16:10:55 -0500

    Bugfix to , as per #232

--- tests/testthat/test_format_yml.R
+++ tests/testthat/test_format_yml.R
@@ -7,6 +7,8 @@ test_that("Export to YAML", {
 
 test_that("Import from YAML", {
     expect_true(is.data.frame(import("iris.yml")))
+    expect_identical(import("iris.yml")[,1:4],iris[,1:4])
+    expect_identical(import("iris.yml")$Species,as.character(iris$Species))
 })
 
 unlink("iris.yml")
