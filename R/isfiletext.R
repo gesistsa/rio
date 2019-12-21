61e6b52ac4bcce068d23a588ec7390b313f701b5
Alex F. Bokov, Ph.D <alex.bokov@gmail.com>
2019-12-20 20:13:29 -0600

    Incorporating review feedback

--- R/isfiletext.R
+++ R/isfiletext.R
@@ -1,9 +1,5 @@
-#' Determine whether a file is "plain-text" or some sort of binary format
+#' @title Determine whether a file is "plain-text" or some sort of binary format
 #' 
-#' For debugging (i.e. why is a file being deemed non-text) set the option
-#' \code{"rio.is_file_text.debug"} to \code{TRUE}. Then the result returned will 
-#' also have an attribute named \code{"non_text_bytes"} which will be a vector 
-#' of the raw bytes which prevented the file from being identified as text
 #' 
 #' @param file       Path to the file
 #' @param maxsize    Maximum number of bytes to read
@@ -12,7 +8,7 @@
 #'                   following value can be used: 
 #'                   \code{as.raw(c(7:16, 18, 19, 32:127))}
 #'
-#' @return boolean
+#' @return A logical
 #' @export
 #' @examples
 #' library(datasets)
@@ -31,11 +27,5 @@ is_file_text <- function(file, maxsize = Inf,
                    n = min(file.info(file)$size, maxsize))
   close(ff)
 
-  non_text_bytes <- setdiff(bytes, text_bytes)
-  result <- length(non_text_bytes) == 0
-  if (getOption("rio.is_file_text.debug", FALSE)) {
-    attr(result, "non_text_bytes") <- non_text_bytes
-  }
-  
-  return(result)
+  return(length(setdiff(bytes, text_bytes)) == 0)
 }
