52d3bd9fed656912607754dd134f72fc7b1177fe
chainsawriot <chainsawtiney@gmail.com>
2023-08-28 23:57:26 +0200

    Clean the test

--- tests/testthat/test_format_sas.R
+++ tests/testthat/test_format_sas.R
@@ -11,7 +11,7 @@ test_that("Import from SAS (.xpt)", {
 })
 
 test_that("Export SAS (.sas7bdat)", {
-    expect_true(export(mtcars, "mtcars.sas7bdat") %in% dir())
+    expect_true(suppressWarnings(export(mtcars, "mtcars.sas7bdat")) %in% dir())
 })
 
 unlink("mtcars.sas7bdat")
