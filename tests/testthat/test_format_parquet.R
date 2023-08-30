bd67fcf8ba828171f21c66f8a20efca8d7ec2c42
chainsawriot <chainsawtiney@gmail.com>
2023-08-30 15:40:05 +0200

    Fix the arrow test error for arrow 7

--- tests/testthat/test_format_parquet.R
+++ tests/testthat/test_format_parquet.R
@@ -3,7 +3,6 @@ require("datasets")
 
 test_that("Export to and import from parquet", {
     skip_if_not_installed("arrow")
-    skip_if_not(arrow::arrow_available())
     expect_true(export(iris, "iris.parquet") %in% dir())
     expect_true(is.data.frame(import("iris.parquet")))
     unlink("iris.parquet")
