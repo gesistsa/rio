5328dffa5aaf3acdcaa9d446725b574714b0adab
chainsawriot <chainsawtiney@gmail.com>
2023-08-31 17:15:58 +0200

    Fix #327 [no ci] (#329)
    * Improve the examples of `import` #327
    
    * Update examples in `export.R` #327
    
    * Rewrite examples `convert()` #327
    
    * Rewrite example `import_list` #327
    
    * Update examples #327
    
    * Update NEWS.md

--- R/convert.R
+++ R/convert.R
@@ -6,33 +6,30 @@
 #' @param out_opts A named list of options to be passed to [export()].
 #' @return A character string containing the name of the output file (invisibly).
 #' @examples
-#' # create a file to convert
-#' export(mtcars, dta_file <- tempfile(fileext = ".dta"))
-#' 
-#' # convert Stata to CSV and open converted file
-#' convert(dta_file, csv_file <- tempfile(fileext = ".csv"))
-#' head(import(csv_file))
-#' 
-#' # correct an erroneous file format
-#' export(mtcars, csv_file2 <- tempfile(fileext = ".csv"), format = "tsv")
-#' convert(csv_file2, csv_file, in_opts = list(format = "tsv"))
-#' 
-#' # convert serialized R data.frame to JSON
-#' export(mtcars, rds_file <- tempfile(fileext = ".rds"))
-#' convert(rds_file, json_file <- tempfile(fileext = ".json"))
-#' 
-#' # cleanup
-#' unlink(csv_file)
-#' unlink(csv_file2)
-#' unlink(rds_file)
-#' unlink(dta_file)
-#' unlink(json_file)
-#' 
-#' \dontrun{\donttest{
-#' # convert from the command line:
+#' ## For demo, a temp. file path is created with the file extension .dta (Stata)
+#' dta_file <- tempfile(fileext = ".dta")
+#' ## .csv
+#' csv_file <- tempfile(fileext = ".csv")
+#' ## .xlsx
+#' xlsx_file <- tempfile(fileext = ".xlsx")
+#'
+#'
+#' ## Create a Stata data file
+#' export(mtcars, dta_file)
+#'
+#' ## convert Stata to CSV and open converted file
+#' convert(dta_file, csv_file)
+#' import(csv_file)
+#'
+#' ## correct an erroneous file format
+#' export(mtcars, xlsx_file, format = "tsv") ## DON'T DO THIS
+#' ## import(xlsx_file) ## ERROR
+#' ## convert the file by specifying `in_opts`
+#' convert(xlsx_file, xlsx_file, in_opts = list(format = "tsv"))
+#' import(xlsx_file)
+#'
+#' ## convert from the command line:
 #' ## Rscript -e "rio::convert('mtcars.dta', 'mtcars.csv')"
-#' }}
-#' 
 #' @seealso [Luca Braglia](https://lbraglia.github.io/) has created a Shiny app called [rioweb](https://github.com/lbraglia/rioweb) that provides access to the file conversion features of rio through a web browser.
 #' @export
 convert <- function(in_file, out_file, in_opts=list(), out_opts=list()) {
