14a683e77f6fddd056a7467b60e54281ff5c1c79
schochastics <david@schochastics.net>
2023-09-11 16:08:38 +0200

    reduced cyclomatic complexity of set_class

--- R/set_class.R
+++ R/set_class.R
@@ -1,21 +1,37 @@
 set_class <- function(x, class = NULL) {
     if (is.null(class)) {
         return(x)
-    } else if ("data.table" %in% class) {
-        if (inherits(x, "data.table")) {
-            return(x)
-        }
-        return(data.table::as.data.table(x))
-    } else if ("tibble" %in% class || "tbl_df" %in% class || "tbl" %in% class) {
-        if (inherits(x, "tbl")) {
-            return(x)
-        }
-        return(tibble::as_tibble(x))
     }
+
+    if ("data.table" %in% class) {
+        return(.ensure_data_table(x))
+    }
+
+    if (any(c("tibble", "tbl_df", "tbl") %in% class)) {
+        return(.ensure_tibble(x))
+    }
+
+    return(.ensure_data_frame(x))
+}
+
+.ensure_data_table <- function(x) {
+    if (inherits(x, "data.table")) {
+        return(x)
+    }
+    return(data.table::as.data.table(x))
+}
+
+.ensure_tibble <- function(x) {
+    if (inherits(x, "tbl")) {
+        return(x)
+    }
+    return(tibble::as_tibble(x))
+}
+
+.ensure_data_frame <- function(x) {
     out <- structure(x, class = "data.frame")
-    # add row names in case `x` wasn't already a data frame (e.g., matlab list)
     if (!length(rownames(out))) {
-        rownames(out) <- as.character(seq_len(length(out[,1L,drop = TRUE])))
+        rownames(out) <- as.character(seq_len(length(out[, 1L, drop = TRUE])))
     }
     return(out)
 }
