9a81cfe3f0af4135941218c29a845a05d8ad7ee3
schochastics <david@schochastics.net>
2023-09-12 16:18:56 +0200

    removed now redundant fwf tests

--- tests/testthat/test_format_fwf.R
+++ tests/testthat/test_format_fwf.R
@@ -10,15 +10,8 @@ test_that("Import from FWF (read.fwf)", {
     expect_true(is.data.frame(import("iris.fwf", widths = c(3, 3, 3, 3, 1))))
     expect_true(is.data.frame(import("iris.fwf", widths = list(c(3, 3, 3, 3, 1)))))
     expect_true(is.data.frame(import("iris.fwf", widths = c(3, 3, 3, 3, 1), col.names = names(iris))))
-    expect_true(is.data.frame(import("iris.fwf", widths = c(3, 3, 3, 3, 1), col.names = names(iris))))
-    expect_true(is.data.frame(import("iris.txt", widths = c(3, 3, 3, 3, 1), format = "fwf")))
-})
-
-test_that("Import from FWF (read_fwf)", {
-    expect_true(is.data.frame(import("iris.fwf", widths = c(3, 3, 3, 3, 1))))
     expect_true(is.data.frame(import("iris.txt", widths = c(3, 3, 3, 3, 1), format = "fwf")))
     # negative column widths
-    expect_true(is.data.frame(import("iris.fwf", widths = c(-3, 3, 3, 3, 1))))
     expect_true(is.data.frame(import("iris.fwf", widths = c(-3, 3, 3, 3, 1))))
 })
 
