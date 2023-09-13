3d91cd5b9a604b936d1aa84d1f0cfd8baa86f3c0
chainsawriot <chainsawtiney@gmail.com>
2023-09-13 21:02:48 +0200

    Fix #315

--- tests/testthat/test_format_feather.R
+++ tests/testthat/test_format_feather.R
@@ -2,17 +2,14 @@ context("feather imports/exports")
 require("datasets")
 
 test_that("Export to feather", {
-    skip_if_not_installed(pkg="feather")
     expect_true(export(iris, "iris.feather") %in% dir())
 })
 
 test_that("Import from feather", {
-    skip_if_not_installed(pkg="feather")
     expect_true(is.data.frame(import("iris.feather")))
 })
 
 test_that("... correctly passed, #318", {
-    skip_if_not_installed(pkg="feather")
     ## actually feather::write_feather has only two arguments (as of 2023-09-01)
     ## it is more for possible future expansion
     expect_error(export(mtcars, "mtcars.feather", hello = 42))
