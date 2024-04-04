003b6686964bb7b8c352e5c15a66b10e5560362f
chainsawriot <chainsawtiney@gmail.com>
2024-04-04 15:44:48 +0200

    Fix #392
    also partially #380

--- tests/testthat/test_format_parquet.R
+++ tests/testthat/test_format_parquet.R
@@ -3,7 +3,10 @@ require("datasets")
 
 test_that("Export to and import from parquet", {
     skip_if_not_installed("arrow")
-    expect_true(export(iris, "iris.parquet") %in% dir())
-    expect_true(is.data.frame(import("iris.parquet")))
-    unlink("iris.parquet")
+    skip_on_os("mac") ## apache/arrow#40991
+    withr::with_tempfile("iris_path", fileext = ".parquet", code = {
+        export(iris, iris_path)
+        expect_true(file.exists(iris_path))
+        expect_true(is.data.frame(import(iris_path)))
+    })
 })
