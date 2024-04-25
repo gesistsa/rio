2fb1373a497f586dacce095300452ccc5a42cf37
chainsawriot <chainsawtiney@gmail.com>
2024-04-25 21:25:24 +0200

    Fix #354 (#397)
    * Fix #354
    
    But it doesn't fix #395 and #396
    
    * Old R bug on Tar

--- tests/testthat/test_compress.R
+++ tests/testthat/test_compress.R
@@ -9,32 +9,18 @@ test_that("Recognize compressed file types", {
 })
 
 test_that("Export to compressed (zip) / import", {
-    withr::with_tempfile("iris_path", fileext = ".csv.zip", code = {
-        e1 <- export(iris, iris_path)
-        expect_true(file.exists(iris_path))
-        expect_true(is.data.frame(import(iris_path)))
-        expect_true(is.data.frame(import(iris_path)))
-        expect_true(is.data.frame(import(iris_path, which = 1)))
-        base_file_name <- gsub("\\.zip", "", basename(iris_path))
-        expect_true(is.data.frame(import(iris_path, which = base_file_name)))
-    })
+    skip_if(getRversion() <= "4.0")
+    ##formats <- c("zip", "tar", "gzip", "bzip2", "xz")
+    formats <- c("zip", "tar") ## 395 #396
+    for (format in formats) {
+        withr::with_tempfile("iris_path", fileext = paste0(".csv.", format), code = {
+            e1 <- export(iris, iris_path)
+            expect_true(file.exists(iris_path))
+            expect_true(is.data.frame(import(iris_path)))
+            expect_true(is.data.frame(import(iris_path)))
+            expect_true(is.data.frame(import(iris_path, which = 1)))
+            base_file_name <- gsub(paste0("\\.", format), "", basename(iris_path))
+            expect_true(is.data.frame(import(iris_path, which = base_file_name)))
+        })
+    }
 })
-
-test_that("Export to compressed (tar)", {
-    withr::with_tempfile("iris_path", fileext = ".csv.tar", code = {
-        e2 <- export(iris, iris_path)
-        expect_true(file.exists(iris_path))
-        ## tar export does not work due to: https://bugs.r-project.org/bugzilla3/show_bug.cgi?id=16716
-        ##expect_true(is.data.frame(import("iris.csv.tar")))
-    })
-})
-
-## test_that("Import from compressed", {
-##     expect_true(is.data.frame(import("iris.csv.zip")))
-##     expect_true(is.data.frame(import("iris.csv.zip", which = 1)))
-##     expect_true(is.data.frame(import("iris.csv.zip", which = "iris.csv")))
-##     # tar export does not work due to: https://bugs.r-project.org/bugzilla3/show_bug.cgi?id=16716
-##     #expect_true(is.data.frame(import("iris.csv.tar")))
-## })
-
-## unlink("iris.csv.tar")
