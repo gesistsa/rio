6c92c414e2d10384975c6abf62e897f5f1b72014
chainsawriot <chainsawtiney@gmail.com>
2024-04-25 16:28:39 +0200

    All done
    Also fixed a leaky test case in `test_set_class.R`

--- tests/testthat/test_import_list.R
+++ tests/testthat/test_import_list.R
@@ -1,22 +1,22 @@
-context("Test import_list()")
-library("datasets")
-
-export(list(mtcars = mtcars, iris = iris), "data.rdata")
-export(mtcars, "mtcars.rds")
-
 test_that("Data identical (import_list)", {
-    expect_equivalent(import_list(rep("mtcars.rds", 2)), list(mtcars, mtcars))
-    mdat <- rbind(mtcars, mtcars)
-    dat <- import_list(rep("mtcars.rds", 2), rbind = TRUE)
-    expect_true(ncol(dat) == ncol(mdat) + 1)
-    expect_true(nrow(dat) == nrow(mdat))
-    expect_true("_file" %in% names(dat))
+    withr::with_tempfile("mtcars_file", fileext = ".rds", code = {
+        export(mtcars, mtcars_file)
+        expect_equivalent(import_list(rep(mtcars_file, 2)), list(mtcars, mtcars))
+        mdat <- rbind(mtcars, mtcars)
+        dat <- import_list(rep(mtcars_file, 2), rbind = TRUE)
+        expect_true(ncol(dat) == ncol(mdat) + 1)
+        expect_true(nrow(dat) == nrow(mdat))
+        expect_true("_file" %in% names(dat))
+    })
 })
 
 test_that("Import multi-object .Rdata in import_list()", {
-    dat <- import_list("data.rdata")
-    expect_true(identical(dat[[1]], mtcars))
-    expect_true(identical(dat[[2]], iris))
+    withr::with_tempfile("rdata_file", fileext = ".rdata", code = {
+        export(list(mtcars = mtcars, iris = iris), rdata_file)
+        dat <- import_list(rdata_file)
+        expect_true(identical(dat[[1]], mtcars))
+        expect_true(identical(dat[[2]], iris))
+    })
 })
 
 test_that("Import multiple HTML tables in import_list()", {
@@ -28,76 +28,85 @@ test_that("Import multiple HTML tables in import_list()", {
 })
 
 test_that("Import multiple HTML tables in import_list() but with htm #350", {
-    temphtm <- tempfile(fileext = ".htm")
-    file.copy("../testdata/twotables.html", temphtm)
-    dat <- import_list(temphtm)
-    expect_true(identical(dim(dat[[1]]), dim(mtcars)))
-    expect_true(identical(names(dat[[1]]), names(mtcars)))
-    expect_true(identical(dim(dat[[2]]), dim(iris)))
-    expect_true(identical(names(dat[[2]]), names(iris)))
+    withr::with_tempfile("temphtm", fileext = ".htm", code = {
+        temphtm <- tempfile(fileext = ".htm")
+        file.copy("../testdata/twotables.html", temphtm)
+        dat <- import_list(temphtm)
+        expect_true(identical(dim(dat[[1]]), dim(mtcars)))
+        expect_true(identical(names(dat[[1]]), names(mtcars)))
+        expect_true(identical(dim(dat[[2]]), dim(iris)))
+        expect_true(identical(names(dat[[2]]), names(iris)))
+    })
 })
 
-
 test_that("import_list() preserves 'which' names when specified", {
-    export(list(a = mtcars, b = iris), "foo.xlsx")
-    expect_true(identical(names(import_list("foo.xlsx")), c("a", "b")))
-    expect_true(identical(names(import_list("foo.xlsx", which = 1)), "a"))
-    expect_true(identical(names(import_list("foo.xlsx", which = "a")), "a"))
-    expect_true(identical(names(import_list("foo.xlsx", which = 2)), "b"))
-    expect_true(identical(names(import_list("foo.xlsx", which = "b")), "b"))
-    expect_true(identical(names(import_list("foo.xlsx", which = 1:2)), c("a", "b")))
-    expect_true(identical(names(import_list("foo.xlsx", which = 2:1)), c("b", "a")))
-    expect_true(identical(names(import_list("foo.xlsx", which = c("a", "b"))), c("a", "b")))
-    expect_true(identical(names(import_list("foo.xlsx", which = c("b", "a"))), c("b", "a")))
-    unlink("foo.xlsx")
+    withr::with_tempfile("data_file", fileext = ".xlsx", code = {
+        export(list(a = mtcars, b = iris), data_file)
+        expect_true(identical(names(import_list(data_file)), c("a", "b")))
+        expect_true(identical(names(import_list(data_file, which = 1)), "a"))
+        expect_true(identical(names(import_list(data_file, which = "a")), "a"))
+        expect_true(identical(names(import_list(data_file, which = 2)), "b"))
+        expect_true(identical(names(import_list(data_file, which = "b")), "b"))
+        expect_true(identical(names(import_list(data_file, which = 1:2)), c("a", "b")))
+        expect_true(identical(names(import_list(data_file, which = 2:1)), c("b", "a")))
+        expect_true(identical(names(import_list(data_file, which = c("a", "b"))), c("a", "b")))
+        expect_true(identical(names(import_list(data_file, which = c("b", "a"))), c("b", "a")))
+    })
 })
 
 test_that("Import single file via import_list()", {
-    expect_true(identical(import_list("mtcars.rds", rbind = TRUE), mtcars))
+    withr::with_tempfile("data_file", fileext = ".rds", code = {
+        export(mtcars, data_file)
+        expect_true(identical(import_list(data_file, rbind = TRUE), mtcars))
+    })
 })
 
 test_that("Import single file from zip via import_list()", {
-    export(mtcars, "mtcars.csv.zip", format = "csv")
-    expect_true(inherits(import_list("mtcars.csv.zip")[[1L]], "data.frame"))
-    expect_true(inherits(import_list("mtcars.csv.zip", which = 1)[[1L]], "data.frame"))
-    expect_true(inherits(import_list("mtcars.csv.zip", which = "mtcars.csv")[[1L]], "data.frame"))
+    withr::with_tempfile("data_file", fileext = ".csv.zip", code = {
+        export(mtcars, data_file, format = "csv")
+        expect_true(is.data.frame(import_list(data_file)[[1L]]))
+        expect_true(is.data.frame(import_list(data_file, which = 1)[[1L]]))
+        basefile_name <- gsub(".zip$", "", basename(data_file))
+        expect_true(is.data.frame(import_list(data_file, which = basefile_name)[[1L]]))
+    })
 })
 
 test_that("Using setclass in import_list()", {
-    dat1 <- import_list(rep("mtcars.rds", 2), setclass = "data.table", rbind = TRUE)
-    expect_true(inherits(dat1, "data.table"))
-    dat2 <- import_list(rep("mtcars.rds", 2), setclass = "tbl", rbind = TRUE)
-    expect_true(inherits(dat2, "tbl"))
+    withr::with_tempfile("data_file", fileext = ".rds", code = {
+        export(mtcars, data_file)
+        dat1 <- import_list(rep(data_file, 2), setclass = "data.table", rbind = TRUE)
+        expect_true(inherits(dat1, "data.table"))
+        dat2 <- import_list(rep(data_file, 2), setclass = "tbl", rbind = TRUE)
+        expect_true(inherits(dat2, "tbl"))
+    })
 })
 
 test_that("Object names are preserved by import_list()", {
-    export(list(mtcars1 = mtcars[1:10,],
-                mtcars2 = mtcars[11:20,],
-                mtcars3 = mtcars[21:32,]), "mtcars.xlsx")
-    export(mtcars[1:10,],  "mtcars1.csv")
-    export(mtcars[11:20,], "mtcars2.tsv")
-    export(mtcars[21:32,], "mtcars3.csv")
-    expected_names <- c("mtcars1", "mtcars2", "mtcars3")
-    dat_xls <- import_list("mtcars.xlsx")
-    dat_csv <- import_list(c("mtcars1.csv","mtcars2.tsv","mtcars3.csv"))
-
-    expect_identical(names(dat_xls), expected_names)
-    expect_identical(names(dat_csv), expected_names)
-
-    unlink(c("mtcars.xlsx", "mtcars1.csv","mtcars2.tsv","mtcars3.csv"))
+    withr::with_tempdir(code = {
+        export(list(mtcars1 = mtcars[1:10,],
+                    mtcars2 = mtcars[11:20,],
+                    mtcars3 = mtcars[21:32,]), "mtcars.xlsx")
+        export(mtcars[1:10,],  "mtcars1.csv")
+        export(mtcars[11:20,], "mtcars2.tsv")
+        export(mtcars[21:32,], "mtcars3.csv")
+        expected_names <- c("mtcars1", "mtcars2", "mtcars3")
+        dat_xls <- import_list("mtcars.xlsx")
+        dat_csv <- import_list(c("mtcars1.csv","mtcars2.tsv","mtcars3.csv"))
+        expect_identical(names(dat_xls), expected_names)
+        expect_identical(names(dat_csv), expected_names)
+    })
 })
 
 test_that("File names are added as attributes by import_list()", {
-    export(mtcars[1:10,],  "mtcars.csv")
-    export(mtcars[11:20,], "mtcars.tsv")
-    expected_names <- c("mtcars", "mtcars")
-    expected_attrs <- c(mtcars = "mtcars.csv", mtcars = "mtcars.tsv")
-    dat <- import_list(c("mtcars.csv","mtcars.tsv"))
-
-    expect_identical(names(dat), expected_names)
-    expect_identical(unlist(lapply(dat, attr, "filename")), expected_attrs)
-
-    unlink(c("mtcars.csv", "mtcars.tsv"))
+    withr::with_tempdir(code = {
+        export(mtcars[1:10,],  "mtcars.csv")
+        export(mtcars[11:20,], "mtcars.tsv")
+        expected_names <- c("mtcars", "mtcars")
+        expected_attrs <- c(mtcars = "mtcars.csv", mtcars = "mtcars.tsv")
+        dat <- import_list(c("mtcars.csv","mtcars.tsv"))
+        expect_identical(names(dat), expected_names)
+        expect_identical(unlist(lapply(dat, attr, "filename")), expected_attrs)
+    })
 })
 
 test_that("URL #294", {
@@ -120,10 +129,11 @@ test_that("URL #294", {
 test_that("Universal dummy `which` #326", {
     formats <- c("xlsx", "dta", "sav", "csv", "csv2")
     for (format in formats) {
-        tempzip <- tempfile(fileext = paste0(".", format, ".zip"))
-        rio::export(mtcars, tempzip, format = format)
-        expect_warning(rio::import(tempzip), NA)
-        expect_warning(rio::import_list(tempzip), NA)
+        withr::with_tempfile("tempzip", fileext = paste0(".", format, ".zip"), code = {
+            rio::export(mtcars, tempzip, format = format)
+            expect_warning(rio::import(tempzip), NA)
+            expect_warning(rio::import_list(tempzip), NA)
+        })
     }
 })
 
@@ -134,23 +144,31 @@ test_that("Universal dummy `which` (Suggests) #326", {
     skip_on_os("mac") ## apache/arrow#40991
     formats <- c("qs", "parquet", "ods")
     for (format in formats) {
-        tempzip <- tempfile(fileext = paste0(".", format, ".zip"))
-        rio::export(mtcars, tempzip, format = format)
-        expect_warning(rio::import(tempzip), NA)
-        expect_warning(rio::import_list(tempzip), NA)
+        withr::with_tempfile("tempzip", fileext = paste0(".", format, ".zip"), code = {
+            rio::export(mtcars, tempzip, format = format)
+            expect_warning(rio::import(tempzip), NA)
+            expect_warning(rio::import_list(tempzip), NA)
+        })
     }
 })
 
 test_that("Informative message when files are not found #389", {
-    expect_warning(import_list(c("mtcars.rds", "nonexisting.rds")), "^Import failed for nonexisting")
+    withr::with_tempfile("mtcars_file", fileext = ".rds", code = {
+        export(mtcars, mtcars_file)
+        expect_true(file.exists(mtcars_file))
+        expect_false(file.exists("nonexisting.rds"))
+        expect_warning(import_list(c(mtcars_file, "nonexisting.rds")), "^Import failed for nonexisting")
+    })
 })
 
 test_that("Missing files and rbind", {
-    expect_warning(x <- import_list(c("mtcars.rds", "nonexisting.rds"), rbind = TRUE), "^Import failed for nonexisting")
-    expect_warning(x <- import_list(c("nonexisting.rds", "nonexisting2.rds"), rbind = TRUE), "^Import failed for nonexisting")
-    expect_true(is.data.frame(x))
+    withr::with_tempfile("mtcars_file", fileext = ".rds", code = {
+        export(mtcars, mtcars_file)
+        expect_true(file.exists(mtcars_file))
+        expect_false(file.exists("nonexisting.rds"))
+        expect_false(file.exists("nonexisting2.rds"))
+        expect_warning(x <- import_list(c(mtcars_file, "nonexisting.rds"), rbind = TRUE), "^Import failed for nonexisting")
+        expect_true(is.data.frame(x))
+        expect_warning(x <- import_list(c("nonexisting.rds", "nonexisting2.rds"), rbind = TRUE), "^Import failed for nonexisting")
+    })
 })
-
-unlink("data.rdata")
-unlink("mtcars.rds")
-unlink("mtcars.csv.zip")
--- tests/testthat/test_set_class.R
+++ tests/testthat/test_set_class.R
@@ -1,9 +1,9 @@
-context("Set object class")
-library("datasets")
 mtcars_tibble <- tibble::as_tibble(mtcars)
 mtcars_datatable <- data.table::as.data.table(mtcars)
 
 test_that("Set object class", {
+    mtcars_tibble <- tibble::as_tibble(mtcars)
+    mtcars_datatable <- data.table::as.data.table(mtcars)
     expect_true(inherits(set_class(mtcars), "data.frame"))
     expect_true(inherits(set_class(mtcars_tibble), "data.frame"))
     expect_true(inherits(set_class(mtcars_datatable), "data.frame"))
@@ -12,6 +12,8 @@ test_that("Set object class", {
 })
 
 test_that("Set object class as tibble", {
+    mtcars_tibble <- tibble::as_tibble(mtcars)
+    mtcars_datatable <- data.table::as.data.table(mtcars)
     expect_true(inherits(set_class(mtcars, class = "tbl_df"), "tbl_df"))
     expect_true(inherits(set_class(mtcars, class = "tibble"), "tbl_df"))
     expect_true(inherits(set_class(mtcars_tibble, class = "tibble"), "tbl_df"))
@@ -19,10 +21,11 @@ test_that("Set object class as tibble", {
 
 test_that("Set object class as data.table", {
     expect_true(inherits(set_class(mtcars, class = "data.table"), "data.table"))
-    export(mtcars, "mtcars.csv")
-    expect_true(inherits(import("mtcars.csv", setclass = "data.table"), "data.table"))
-    expect_true(inherits(import("mtcars.csv", data.table = TRUE, setclass = "data.table"), "data.table"))
-    unlink("mtcars.csv")
+    withr::with_tempfile("data_file", fileext = ".csv", code = {
+        export(mtcars, data_file)
+        expect_true(inherits(import(data_file, setclass = "data.table"), "data.table"))
+        expect_true(inherits(import(data_file, data.table = TRUE, setclass = "data.table"), "data.table"))
+    })
 })
 
 test_that("Set object class as arrow table", {
@@ -32,27 +35,31 @@ test_that("Set object class as arrow table", {
     expect_false(inherits(set_class(mtcars_arrow), "data.frame")) ## arrow table is not data.frame
     expect_true(inherits(set_class(mtcars, class = "arrow"), "ArrowTabular"))
     expect_true(inherits(set_class(mtcars, class = "arrow_table"), "ArrowTabular"))
-    export(mtcars, "mtcars.csv")
-    expect_true(inherits(import("mtcars.csv", setclass = "arrow"), "ArrowTabular"))
-    expect_true(inherits(import("mtcars.csv", data.table = TRUE, setclass = "arrow"), "ArrowTabular"))
-    unlink("mtcars.csv")
+    withr::with_tempfile("data_file", fileext = ".csv", code = {
+        export(mtcars, data_file)
+        expect_true(inherits(import(data_file, setclass = "arrow"), "ArrowTabular"))
+        expect_true(inherits(import(data_file, data.table = TRUE, setclass = "arrow"), "ArrowTabular"))
+    })
 })
 
 test_that("ArrowTabular can be exported", {
     skip_if(getRversion() <= "4.2")
     skip_if_not_installed("arrow")
     mtcars_arrow <- arrow::arrow_table(mtcars)
-    expect_error(export(mtcars_arrow, "mtcars.csv"), NA) ## no concept of rownames
-    expect_true(inherits(import("mtcars.csv"), "data.frame"))
-    unlink("mtcars.csv")
+    withr::with_tempfile("data_file", fileext = ".csv", code = {
+        expect_error(export(mtcars_arrow, data_file), NA) ## no concept of rownames
+        expect_true(inherits(import(data_file), "data.frame"))
+    })
 })
 
 test_that("Simulate arrow is not installed, #376", {
     ## although this is pretty meaningless
-    with_mocked_bindings({
-        export(mtcars, "mtcars.csv")
-        expect_error(import("mtcars.csv", setclass = "arrow"), "Suggested package")
-    }, .check_pkg_availability = function(pkg, lib.loc = NULL) {
-        stop("Suggested package `", pkg, "` is not available. Please install it individually or use `install_formats()`", call. = FALSE)
+    withr::with_tempfile("data_file", fileext = ".csv", code = {
+        with_mocked_bindings({
+            export(mtcars, data_file)
+            expect_error(import(data_file, setclass = "arrow"), "Suggested package")
+        }, .check_pkg_availability = function(pkg, lib.loc = NULL) {
+            stop("Suggested package `", pkg, "` is not available. Please install it individually or use `install_formats()`", call. = FALSE)
+        })
     })
 })
