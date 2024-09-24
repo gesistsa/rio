991ec868903a7fd421adfb8e16c3fc7447efe415
chainsawriot <chainsawtiney@gmail.com>
2024-09-24 10:13:52 +0200

    Update onLoad (WIP)

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,7 +2,7 @@
     if (interactive()) {
         w <- uninstalled_formats()
         if (length(w)) {
-            msg <- "The following rio suggested packages are not installed: %s\nUse 'install_formats()' to install them"
+            msg <- "Some optional R packages were not installed and therefore some file formats are not supported. Check file support with show_supported_formats()"
             packageStartupMessage(sprintf(msg, toString(sQuote(w))))
         }
     }
