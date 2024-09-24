dcf7eb1ad96240497544f2cd1e85e69f41920ec1
chainsawriot <chainsawtiney@gmail.com>
2024-09-24 14:54:34 +0200

    More tests

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,8 +2,8 @@
     if (interactive()) {
         w <- uninstalled_formats()
         if (length(w)) {
-            msg <- "Some optional R packages were not installed and therefore some file formats are not supported. Check file support with show_supported_formats()"
-            packageStartupMessage(sprintf(msg, toString(sQuote(w))))
+            msg <- "Some optional R packages were not installed and therefore some file formats are not supported. Check file support with show_unsupported_formats()"
+            packageStartupMessage(msg)
         }
     }
 }
--- tests/testthat/test_suggestions.R
+++ tests/testthat/test_suggestions.R
@@ -11,7 +11,11 @@ test_that("uninstalled_formats()", {
 
 test_that("show_unsupported_formats (in the fully supported environment) on CI", {
     skip_on_cran()
+    for (pkg in attr(rio_formats, "suggested_packages")) {
+        skip_if_not_installed(pkg)
+    }
     expect_false(show_unsupported_formats())
+    expect_message(show_unsupported_formats(), "All default")
 })
 
 test_that("show_unsupported_formats (in the partial supported environment) on CI", {
@@ -21,5 +25,6 @@ test_that("show_unsupported_formats (in the partial supported environment) on CI
     }
     with_mocked_bindings(code = {
         expect_true(show_unsupported_formats())
+        expect_message(show_unsupported_formats(), "These formats are not supported")
     }, `uninstalled_formats` = fake_uninstalled_formats)
 })
