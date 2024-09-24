96a58dca2dccec92fa2b955797e6a8271adcb8a0
chainsawriot <chainsawtiney@gmail.com>
2024-09-24 14:45:23 +0200

    Add tests
    the second one is a mocking test

--- tests/testthat/test_suggestions.R
+++ tests/testthat/test_suggestions.R
@@ -7,3 +7,19 @@ test_that("uninstalled_formats()", {
         expect_type(formats, "character")
     }
 })
+
+
+test_that("show_unsupported_formats (in the fully supported environment) on CI", {
+    skip_on_cran()
+    expect_false(show_unsupported_formats())
+})
+
+test_that("show_unsupported_formats (in the partial supported environment) on CI", {
+    skip_on_cran()
+    fake_uninstalled_formats <- function() {
+        return(c("readODS"))
+    }
+    with_mocked_bindings(code = {
+        expect_true(show_unsupported_formats())
+    }, `uninstalled_formats` = fake_uninstalled_formats)
+})
